89d12617853dc9d6f77c07feaa140d0880cb9250
elvar3278 <111759185+elvar3278@users.noreply.github.com>
2024-04-29 15:11:26 +0000

    Update Skipti.cs

--- Skipti.cs
+++ Skipti.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,21 +7,28 @@ public class Skipti : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log("byrja");
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        other.gameObject.SetActive(false);
-        StartCoroutine(Bida());    
-    }
-    IEnumerator Bida()
-    {
-        yield return new WaitForSeconds(3);
-        Endurræsa();
-    }
-    public void Endurræsa()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);//næsta sena
+        Debug.Log("byrja"); // Output "byrja" to the console when the script starts
     }
 
+    // Triggered when another Collider enters the trigger zone of this GameObject
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false); // Deactivate the GameObject that entered the trigger zone
+        StartCoroutine(Bida()); // Start a coroutine named Bida
+    }
+
+    // Coroutine function to wait for a specified duration before proceeding
+    IEnumerator Bida()
+    {
+        yield return new WaitForSeconds(3); // Wait for 3 seconds
+        Endurræsa(); // Call the Endurræsa method after waiting
+    }
+
+    // Method to load the next scene in the build index
+    public void Endurræsa()
+    {
+        // Load the next scene in the build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
+
